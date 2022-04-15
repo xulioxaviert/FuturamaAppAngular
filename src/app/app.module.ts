cf8dbf0b6523fd7ba9054754012be4ee341f18b1
Xulio Xaviert Rojas Teixeira <xulioxaviert@gmail.com>
2022-04-15 17:01:42 +0200

    chore: added styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { EpisodesComponent } from './pages/episodes/episodes.component';
 import { HeaderComponent } from './pages/header/header.component';
 import { CastComponent } from './pages/cast/cast.component';
 import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './pages/footer/footer.component';
 
 
 @NgModule({
@@ -21,7 +22,8 @@ import { HomeComponent } from './pages/home/home.component';
     HeaderComponent,
     CastComponent,
     LinkPipe,
-    HomeComponent
+    HomeComponent,
+    FooterComponent
     
   ],
   imports: [
